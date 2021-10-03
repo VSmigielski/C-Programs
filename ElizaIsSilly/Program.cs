b717740bc34b0d5d9c59433cca9e53966366c9d9
Veronica <veronicasmigielski@gmail.com>
2021-10-03 16:03:07 +0200

    Added Eliza to project

--- ElizaIsSilly/Program.cs
+++ ElizaIsSilly/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ElizaIsSilly
 {
@@ -10,6 +6,8 @@ namespace ElizaIsSilly
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Doctor.Intro());
+
         }
     }
 }
