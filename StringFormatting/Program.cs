e691031349bc2a868e237c0298580003a174937c
Veronica <veronicasmigielski@gmail.com>
2021-10-04 13:08:01 +0200

    Formatting and Precision with numbers

--- StringFormatting/Program.cs
+++ StringFormatting/Program.cs
@@ -10,20 +10,31 @@ namespace StringFormatting
     {
         static void Main(string[] args)
         {
-            string apples = "Apples";
-            int appleQuantity = 8;
-            int applePrice = 100;
-            string oranges = "Oranges";
-            int orangeQuantity = 12;
-            int orangePrice = 80;
+            //string apples = "Apples";
+            //int appleQuantity = 8;
+            //int applePrice = 100;
+            //string oranges = "Oranges";
+            //int orangeQuantity = 12;
+            //int orangePrice = 80;
 
-            string column1Heading = "Fruit";
-            string column2Heading = "Quantity";
-            string column3Heading = "Price";
+            //string column1Heading = "Fruit";
+            //string column2Heading = "Quantity";
+            //string column3Heading = "Price";
 
-            Console.WriteLine($"{column1Heading, -12} {column2Heading, 8} {column3Heading, 6}");
-            Console.WriteLine($"{apples, -12} {appleQuantity, -8} {applePrice, 6} cents");
-            Console.WriteLine($"{oranges, -12} {orangeQuantity, -8} {orangePrice, 6} cents");
+            //// 12, 8, 6 is width in characters 
+            //// Negative writes to the left for the names of items
+            //// " :C " gives currency
+            //// You can use expressions within the {} on variables
+            //Console.WriteLine($"{column1Heading, -12} {column2Heading, 8} {column3Heading, 10}");
+            //Console.WriteLine($"{apples, -12} {appleQuantity, -8} {applePrice/100.0, 10:C}");
+            //Console.WriteLine($"{oranges, -12} {orangeQuantity, -8} {orangePrice/100.0, 10:C}");
+
+            Console.WriteLine($"PI is {Math.PI}");
+
+            Console.WriteLine($"PI is {Math.PI:F3}");
+            Console.WriteLine($"PI is {Math.PI:F0}");
+            Console.WriteLine($"PI is {Math.PI:F12}");
+            Console.WriteLine($"PI is {Math.PI:F99}");
         }
     }
 }
