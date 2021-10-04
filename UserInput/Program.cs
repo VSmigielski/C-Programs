11f010837b8e25363a3aa025d71d3606993ba331
Veronica <veronicasmigielski@gmail.com>
2021-10-04 12:48:15 +0200

    UserInput Project

---
+++ UserInput/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name + ".");
+            Console.WriteLine("How old are you?");
+            var age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(name + " is " + age + " years old.")
+        }
+    }
+}
