98aceb242ff0d895891b46f366d4085a3beb2ec7
Veronica <veronicasmigielski@gmail.com>
2021-12-18 11:23:23 +0100

    Learned how to swap an if/else to the ternary operator

---
+++ Ternary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ternary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What day is it?");
+
+            string day = Console.ReadLine();
+
+            string howToSpendTime;
+
+            /*if (day.ToLower().Equals("saturday"))
+            {
+                howToSpendTime = "Relax";
+            } else
+            {
+                howToSpendTime = "Work";
+            }*/
+
+            howToSpendTime = day.ToLower().Equals("saturday") ? "Relax" : "Work";
+
+            Console.WriteLine($"{howToSpendTime} on {day}");
+        }
+    }
+}
