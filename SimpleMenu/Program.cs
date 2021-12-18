bbfeafbd09bdfea64530cc71e385a90dbe11f8dc
Veronica <veronicasmigielski@gmail.com>
2021-12-18 10:46:37 +0100

    Simple Menu Switch Statement with Default

--- SimpleMenu/Program.cs
+++ SimpleMenu/Program.cs
@@ -24,6 +24,39 @@ namespace SimpleMenu
             {
                 userChoice = char.ToLower(Console.ReadKey(true).KeyChar);
                 Console.WriteLine("You chose " + userChoice);
+            
+                switch (userChoice)
+                {
+                    case '1':
+                        Console.WriteLine("Making Cappuccino...");
+                        Console.WriteLine("Steam the milk");
+                        Console.WriteLine("Froth the milk");
+                        Console.WriteLine("Make espresso");
+                        Console.WriteLine("Add the milk to the espresso");
+                        break;
+                    case '2':
+                        Console.WriteLine("Making Latte...");
+                        Console.WriteLine("Make espresso");
+                        Console.WriteLine("Steam the milk");
+                        Console.WriteLine("Add milk to the espresso");
+                        break;
+                    case '3':
+                        Console.WriteLine("Making Americano...");
+                        break;
+                    case '4':
+                        Console.WriteLine("Making Mocha...");
+                        break;
+                    case '5':
+                        Console.WriteLine("Making Macchiatto...");
+                        break;
+                    case '6':
+                        Console.WriteLine("Making Espresso...");
+                        break;
+                    default:
+                        Console.WriteLine("Returning coins");
+                        break;
+                }
+            
             } while (!userChoice.Equals('q'));
 
 
