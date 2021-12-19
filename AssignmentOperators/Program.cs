cb0f4d95bd79a4a34e38fe3a10b398cfe6192306
Veronica <veronicasmigielski@gmail.com>
2021-12-19 13:23:42 +0100

    Finished assignment operators

--- AssignmentOperators/Program.cs
+++ AssignmentOperators/Program.cs
@@ -6,7 +6,11 @@ namespace AssignmentOperators
     {
         static void Main(string[] args)
         {
-            int x = 12;
+            int x = 1;
+            int y = 1;
+            bool check = (x != y == !(x == y));
+
+            x = 12;
 
             x += 34;
             Console.WriteLine(x);
@@ -17,12 +21,17 @@ namespace AssignmentOperators
             x *= 34;
             Console.WriteLine(x);
 
-            int y = 12;
+            y = 12;
             y = y * 34;
             Console.WriteLine(y);
 
             int z = y -= 8;
             Console.WriteLine($"y is {y}, z is {z}");
+
+            double a = 1 + 3 * 15 / 2;
+            double b = 1 + 3 * 15 / 2.0;
+            Console.WriteLine($"{a}, {b}");
+
         }
     }
 }
